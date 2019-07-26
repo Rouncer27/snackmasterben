a06c02ace4e3403a74ae25c0535a7a06329f53d8
Trevor Rounce <trevorrounce@Trevors-iMac.local>
2019-07-26 09:09:30 -0600

    styling

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -7,13 +7,14 @@ import { colors, fontSizer } from "../Utilities/"
 
 const EpisodeWrapper = styled.div`
   width: 100%;
+  margin: 4rem auto;
   transition: all 0.3s ease-in-out;
   box-shadow: 0 0 1px 1px #f3ffbd;
   box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.5);
 
   @media (min-width: 768px) {
     width: calc(33.33% - 20px);
-    margin: 10px;
+    margin: 1rem;
   }
 
   &:hover {
@@ -63,9 +64,13 @@ const EpisodeWrapper = styled.div`
     }
 
     p {
-      position: relative;
+      position: absolute;
+      top: 50%;
+      left: 50%;
       width: 100%;
       margin: 0;
+      padding: 2rem;
+      transform: translate(-50%, -50%);
       color: #fff;
       font-size: 22px;
       text-align: center;
@@ -79,14 +84,14 @@ const EpisodeWrapper = styled.div`
       }
 
       &::before {
-        top: -4rem;
-        left: -2rem;
+        top: -5rem;
+        left: 1rem;
         content: "“";
       }
 
       &::after {
-        bottom: -6rem;
-        right: -2rem;
+        bottom: -9rem;
+        right: 1rem;
         content: "”";
       }
     }
@@ -95,13 +100,15 @@ const EpisodeWrapper = styled.div`
   .image-section {
     position: relative;
     width: 100%;
-    height: 250px;
+    height: 25rem;
 
     h3 {
-      ${fontSizer(2, 3, 76.8, 110)}
+      ${fontSizer(2, 2.4, 76.8, 110)}
       position: absolute;
       bottom: 1rem;
       left: 1rem;
+      margin: 0;
+      line-height: 1;
       color: #fff;
       z-index: 10;
     }
